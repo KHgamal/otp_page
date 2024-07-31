249ea31b9b745b0fd89d11b451a6a212197bc673
Dell <khokha122002kh@gmail.com>
2024-07-31 13:13:33 +0300

    add custom sound for notification

--- lib/core/utils/helpers/firebase_messaging_service.dart
+++ lib/core/utils/helpers/firebase_messaging_service.dart
@@ -1,27 +1,59 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:injectable/injectable.dart';
+import 'package:otp_page/core/utils/helpers/di/injectable_config.dart';
 
 @singleton
 class FirebaseMessagingService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   Future<void> initialize() async {
     await _firebaseMessaging.requestPermission();
     String? token = await _firebaseMessaging.getToken();
     print("Device Token: $token");
 
+    // Handle foreground message
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       print("Message received in foreground: ${message.notification?.title}");
-      // Handle foreground message
+       _showNotification(message);
     });
-
+    // Handle background/terminated message click
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
       print("Message clicked!: ${message.notification?.title}");
-      // Handle background/terminated message click
     });
+
+        // Initialize local notifications
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _showNotification(RemoteMessage message) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+      'notification_id',
+      'NotificationName',
+      sound: RawResourceAndroidNotificationSound('custom_sound'),
+      importance: Importance.max,
+      priority: Priority.high,
+    );
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      message.data.hashCode,
+      message.notification?.title,
+      message.notification?.body,
+      platformChannelSpecifics,
+    );
   }
 }
 
+
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+    final FirebaseMessagingService notificationService = getIt<FirebaseMessagingService>();
+  notificationService._showNotification(message);
   print("Handling a background message: ${message.messageId}");
 }
